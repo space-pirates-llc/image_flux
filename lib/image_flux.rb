ff0ba3506e71dadb0e6a2c730529f750a0973bf2
Sho Kusano <sho-kusano@space-pirates.jp>
2017-11-30 16:14:01 +0900

    Implement default_origin

--- lib/image_flux.rb
+++ lib/image_flux.rb
@@ -3,3 +3,13 @@
 require 'image_flux/version'
 require 'image_flux/origin'
 require 'image_flux/option'
+
+module ImageFlux
+  class << self
+    attr_accessor :default_origin
+
+    def image_url(path, options = {})
+      default_origin.image_url(path, options)
+    end
+  end
+end
