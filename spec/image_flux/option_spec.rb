7fceb229723d44e0e384d5fd9cd9b66756bd7795
klriutsa <hiroya.kurushima@litalico.co.jp>
2019-03-05 17:03:49 +0900

    :heavy_plus_sign: Add aspect enum

--- spec/image_flux/option_spec.rb
+++ spec/image_flux/option_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 
 RSpec.describe ImageFlux::Option do
-  subject(:option) { described_class.new(width: 100, o: false, f: 'png') }
+  subject(:option) { described_class.new(width: 100, o: false, f: 'png', a: :crop) }
 
   describe '#to_query' do
     subject(:to_query) { option.to_query }
 
-    it { is_expected.to eq('w=100,o=0,f=png') }
+    it { is_expected.to eq('w=100,o=0,f=png,a=2') }
   end
 end
